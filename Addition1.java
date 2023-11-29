f8595775218b3edccb51b8fbdb9d95261bd87458
Bijayananda Lenka <bijayananda6061@gmail.com>
2023-11-29 10:03:44 +0530

    Updated read file using File object method

--- Addition1.java
+++ Addition1.java
@@ -1,9 +1,13 @@
- class Addition1 {
+import java.io.File;
+class Addition1 {
     public static void main(String[] args) {
         String a = args[0];
+        File file = new File(args[1]);
+        Long Print_Length = file.length();
+
         
 
-        System.out.println("Result: "+ a);
+        System.out.println(" "+Print_Length+" "+args[1]);
     }
     
 }
